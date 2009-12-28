6534394a63f61df5a70d2c6599592dc7159c6b40
Stig Brautaset <stig@brautaset.org>
2009-12-28 17:36:17 +0000

    Cocoa Touch templates

---
+++ Objective-C class/UITableViewCell subclass.pbfiletemplate/class.m
@@ -0,0 +1,34 @@
+//
+//  «FILENAME»
+//  «PROJECTNAME»
+//
+//  Created by «FULLUSERNAME» on «DATE».
+//  Copyright «YEAR» «ORGANIZATIONNAME». All rights reserved.
+//
+
+«OPTIONALHEADERIMPORTLINE»
+
+@implementation «FILEBASENAMEASIDENTIFIER»
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    if (self = [super initWithStyle:style reuseIdentifier:reuseIdentifier]) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
