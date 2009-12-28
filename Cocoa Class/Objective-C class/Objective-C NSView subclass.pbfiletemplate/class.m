7c3ca622f9bf19f591ecec22a3c17cb8d1904915
Stig Brautaset <stig@brautaset.org>
2009-12-28 17:44:07 +0000

    add plain Cocoa file templates

---
+++ Cocoa Class/Objective-C class/Objective-C NSView subclass.pbfiletemplate/class.m
@@ -0,0 +1,25 @@
+//
+//  «FILENAME»
+//  «PROJECTNAME»
+//
+//  Created by «FULLUSERNAME» on «DATE».
+//  Copyright «YEAR» «ORGANIZATIONNAME». All rights reserved.
+//
+
+«OPTIONALHEADERIMPORTLINE»
+
+@implementation «FILEBASENAMEASIDENTIFIER»
+
+- (id)initWithFrame:(NSRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect {
+    // Drawing code here.
+}
+
+@end
---
+++ Cocoa Class/Objective-C test case class.pbfiletemplate/class.h
@@ -0,0 +1,16 @@
+//
+//  «FILENAME»
+//  «PROJECTNAME»
+//
+//  Created by «FULLUSERNAME» on «DATE».
+//  Copyright «YEAR» «ORGANIZATIONNAME». All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface «FILEBASENAMEASIDENTIFIER» : SenTestCase {
+
+}
+
+@end
