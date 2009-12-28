4d8e43fa85d39e045842ded3928ca93a9985d703
Stig Brautaset <stig@brautaset.org>
2009-12-28 18:32:41 +0000

    better install?

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+base=~/Library/Application\ Support/Developer/Shared/Xcode
+mkdir -p "$base"
+
 for f in "File Templates" ; do
     src=`pwd`/$f
-    dst=~/Library/Application\ Support/Developer/Shared/Xcode/$f
-    echo "Linking $src to $dst"
-    ln -sf "$src" "$dst"
+
+    # Where to link the files.
+    echo "Linking: $src"
+    echo "     to: $base"
+	ln -sf "$src" "$base"
 done
